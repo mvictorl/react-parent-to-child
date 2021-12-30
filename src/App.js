3047e744f169a7ed7d517ed1d4c0742f4187219e
mvictorl <mvictorl@gmail.com>
2021-12-30 15:01:19 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Parent } from './components/Parent.jsx';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import './style.css';
 
 export default function App() {
